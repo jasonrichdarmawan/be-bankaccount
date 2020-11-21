d8e398a63a531a2cf68e56b5dce400a37df0507e
Jason <jason.onggo@tempatkerja.com>
2020-11-21 17:54:36 +0700

    add LoginController

--- README.md
+++ README.md
@@ -15,6 +15,11 @@ To reduce boilerplate, follow this coding styles.
     this.fullName = fullName;
   }
   ```
+- Use @JsonProperty("attribute")
+  ```
+  @JsonProperty("Full_Name")
+  String fullName;
+  ```
 
 ### Model used by @Mapper
 @Mapper can convert `ResultSet` to Object without setters and constructor:
---
+++ src/main/java/com/personal/bebankaccount/service/LoginService.java
@@ -0,0 +1,61 @@
+package com.personal.bebankaccount.service;
+
+import com.personal.bebankaccount.mapper.User_InfoMapper;
+import com.personal.bebankaccount.mapper.User_LoginMapper;
+import com.personal.bebankaccount.model.LoginModel;
+import com.personal.bebankaccount.model.User_InfoModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class LoginService {
+
+  @Autowired
+  User_LoginMapper user_loginMapper;
+
+  @Autowired
+  User_InfoMapper user_infoMapper;
+
+  @Autowired
+  HashedPINService hashedPINService;
+
+  @Autowired
+  JWTService jwtService;
+
+  public Map<String, Object> authenticate(LoginModel loginModel) {
+    Map<String, Object> body = new HashMap<>();
+    String hashedPIN = user_loginMapper.getHashedPIN(loginModel.getUser_ID());
+
+    if (hashedPIN == null) {
+      body.put("message_code", 302);
+      body.put("message", "Not Found");
+    } else {
+      boolean isValid = hashedPINService.validate(loginModel.getPIN(), hashedPIN);
+
+      if (!isValid) {
+        body.put("message_code", 302);
+        body.put("message", "Not Found");
+      } else {
+        User_InfoModel user_infoModel = user_infoMapper.selectByUser_ID(loginModel.getUser_ID());
+
+        if (user_infoModel == null) {
+          body.put("message_code", 500);
+          body.put("message", "User_Info is not found. Contact Customer Support 1500888");
+        } else {
+          String token = jwtService.generate(user_infoModel.getAccount_Number(), loginModel.getUser_ID());
+
+          body.put("message_code", 200);
+          body.put("message", "OK");
+          body.put("Full_Name", user_infoModel.getFull_Name());
+          body.put("ISO_4217", user_infoModel.getISO_4217());
+          body.put("token", token);
+        }
+      }
+    }
+
+    return body;
+  }
+}
