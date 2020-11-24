e8474cbabf7ee2868eb038e6aed7c4bf9b3af7ec
Jason <jason.onggo@tempatkerja.com>
2020-11-24 23:47:49 +0700

    refactor the mutation mapper

--- src/main/java/com/personal/bebankaccount/mapper/TransactionsMapper.java
+++ src/main/java/com/personal/bebankaccount/mapper/TransactionsMapper.java
@@ -13,10 +13,17 @@ import java.util.List;
 @Mapper
 public interface TransactionsMapper {
 
-  @Select("SELECT" +
-          "\n(SELECT sum(Transaction_Value) FROM transactions WHERE Destination=#{accountNumber} AND Date BETWEEN #{start} AND #{end})" +
-          "\n-" +
-          "\nIFNULL((SELECT sum(Transaction_Value) FROM transactions WHERE Source=#{accountNumber} AND Date BETWEEN #{start} AND #{end}), 0)")
+//  @Select("SELECT" +
+//          "\n(SELECT sum(Transaction_Value) FROM transactions WHERE Destination=#{accountNumber} AND Date BETWEEN #{start} AND #{end})" +
+//          "\n-" +
+//          "\nIFNULL((SELECT sum(Transaction_Value) FROM transactions WHERE Source=#{accountNumber} AND Date BETWEEN #{start} AND #{end}), 0)")
+  @Select("SELECT sum(CASE WHEN Destination = #{accountNumber} THEN Transaction_Value\n" +
+          "                WHEN Source = #{accountNumber} THEN -Transaction_Value\n" +
+          "                ELSE 0\n" +
+          "           END)\n" +
+          "FROM transactions\n" +
+          "WHERE Date BETWEEN #{start} AND #{end} AND\n" +
+          "      #{accountNumber} IN (Destination, Source)")
   BigDecimal mutation(String accountNumber, LocalDate start, LocalDate end);
 
   @Insert("INSERT INTO transactions (Date, Source, Destination, Destination_Type, Transaction_Value) VALUES (#{transferModel.Date}, #{source}, #{transferModel.destination}, #{transferModel.destinationType}, #{transferModel.transactionValue})")
